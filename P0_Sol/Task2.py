8c2a3b31939882db8da69b673e423746241c693c
mandeepak <bmandeepak@users.noreply.github.com>
2019-05-07 01:08:44 -0700

    Fixed the bug

--- P0_Sol/Task2.py
+++ P0_Sol/Task2.py
@@ -29,15 +29,30 @@ month = None
 year = None
 
 # Code Complexity is O(n)
+total_time = {}
 for row in calls:
-	telephone_no = row[0]
+	outgoing_telephone_no = row[0]
+	incoming_telephone_no = row[1]
+	
 	date_time_str = row[2]
 	date_time_obj = datetime.datetime.strptime(date_time_str, '%d-%m-%Y %H:%M:%S')
 	month = datetime.date(1900, date_time_obj.month, 1).strftime('%B')
 	year = date_time_obj.year
 	time_sec  = int(row[3])
-	if max_call_time<time_sec:
-		max_call_time = time_sec
-		month = month
-		telephone_no = telephone_no
-print('{} spent the longest time, {} seconds, on the phone during {} {}.'.format(telephone_no,max_call_time,month,year))
+	if outgoing_telephone_no not in total_time.keys():
+		total_time[outgoing_telephone_no] = time_sec
+	else:
+		total_current_time = total_time[outgoing_telephone_no]
+		total_current_time += time_sec
+		total_time[outgoing_telephone_no] = total_current_time
+
+	if incoming_telephone_no not in total_time.keys():
+		total_time[incoming_telephone_no] = time_sec
+	else:
+		total_current_time = total_time[incoming_telephone_no]
+		total_current_time += time_sec
+		total_time[incoming_telephone_no] = total_current_time
+
+sorted_total_time = sorted(total_time.items(), key=lambda x: x[1],reverse=True)
+
+print('{} spent the longest time, {} seconds, on the phone during September 2016.'.format(sorted_total_time[0][0],sorted_total_time[0][1]))
